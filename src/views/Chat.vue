59bd3ca277f3aec57ff69b785b5a8992b4ab2ff8
Kare-Udon <gpg@udon.eu.org>
2023-03-17 23:28:23 +0800

    update

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,27 +1,20 @@
 <template>
-  
-
-    <div>
-      <v-card width="300">
+  <div>
+    <v-card width="300">
       <v-card-text>
-        <p>{{chatstring}}</p>
+        <p>{{ chatstring }}</p>
       </v-card-text>
-      </v-card>
-  </div>  
-
+    </v-card>
+  </div>
 </template>
 
+<script setup>
+import { useRoute } from "vue-router";
+import { ref, onMounted } from "vue";
+const route = useRoute();
 
-<script>
-import {useRoute} from 'vue-router'
-import {ref,onMounted,defineComponent} from 'vue' 
-const route = useRoute()
-
-const chatstring = ref('')
-onMounted(()=>{chatstring.value = route.query.chatstring})
-export default defineComponent({
-  name: 'Chat',
-  
- 
+const chatstring = ref("");
+onMounted(() => {
+  chatstring.value = route.query.chatstring;
 });
 </script>
